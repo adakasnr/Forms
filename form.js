d4676e13c58d30f88468a7acff64c348f8ecfd69
adakasnr <adakasnr@gmail.com>
2024-03-09 23:33:00 +0530

    Form Validation

--- form.js
+++ form.js
@@ -1,44 +1,105 @@
 const submit = (document.getElementById('submit').onclick = function () {
+    // username
     const username = document.getElementById('username').value;
+    const username_error = document.getElementById('username_error');
     console.log(username);
-    const Password = document.getElementById('Password').value;
-    console.log(Password);
-    const fullName = document.getElementById('fullName').value;
-    console.log(fullName);
-    const LastName = document.getElementById('LastName').value;
-    console.log(LastName);
-    const Gender = document.getElementById('Gender').value;
-    console.log(Gender);
-    const Email = document.getElementById('Email').value;
-    console.log(Email);
-    const passportAvailability = document.querySelector('input[name="options"]:checked').value;
-    console.log("Passport availability:", passportAvailability);
+    if (username.length == 0) {
+        username_error.innerHTML = "username! is requried";
+    } else {
+        username_error.innerHTML = "";
+    };
 
-    const checkboxes = document.querySelectorAll('input[name="option"]');
-    const checkedDetails = [];
-    
+    // password
+    const Password = document.getElementById('Password').value;
+    const Password_error = document.getElementById('Password_error');
+    console.log(Password);
+    if (Password.length == 0) {
+        Password_error.innerHTML = "Enter Password";
+    } else {
+        Password_error.innerHTML = "";
+    }
+
+    // fullname
+    const fullName = document.getElementById('fullName').value;
+    const fullName_error = document.getElementById('fullName_error');
+    console.log(fullName);
+    if (fullName.length == 0) {
+        fullName_error.innerHTML = "Please Mention your fullname"
+    } else {
+        fullName_error.innerHTML = ""
+    }
+
+    // lastname
+    const LastName = document.getElementById('LastName').value;
+    const LastName_error = document.getElementById('LastName_error');
+    console.log(LastName);
+    if (LastName.length == 0) {
+        LastName_error.innerHTML = "Please Mention your lastname"
+    } else {
+        LastName_error.innerHTML = ""
+    }
+
+    // gender
+    const Gender = document.getElementById('Gender').value;
+    const Gender_error = document.getElementById('Gender_error')
+    console.log("Gender", Gender);
+    if (Gender == "none") {
+        Gender_error.innerHTML = "select Gender"
+    } else {
+        Gender_error.innerHTML = ""
+    }
+    // DOB
+    const DOB = document.getElementById('DOB').value;
+    const DOB_error = document.getElementById('DOB_error')
+    console.log("DOB",DOB);
+    if(DOB == 0 ){
+        DOB_error.innerHTML = "Date of Birth is Compulsary"
+    }else{
+        DOB_error.innerHTML = ""
+    }
+
+
+    // email
+    const Email = document.getElementById('Email').value;
+    const Email_error = document.getElementById('Email_error')
+    console.log(Email);
+    if (username.length == 0) {
+        Email_error.innerHTML = "enter Email";
+    } else {
+        Email_error.innerHTML = "";
+    };
+
+    // passport
+    const passportAvailability = document.querySelector('input[name="options"]:checked');
+    debugger
+    const passport_error = document.getElementById('passport_error')
+    console.log("Passport availability:", passportAvailability);
+    if (passportAvailability){
+        passport_error.innerHTML = ""
+    }else {
+        passport_error.innerHTML = "please your Passport availability"
+    }
+
+    // checkbox
+    const checkboxes = document.querySelectorAll('input[name="Education"]');
+    let checkedDetails = [];
+    checkboxes.forEach(element => {
+        if (element.checked) {
+            checkedDetails.push(element.value)
+        }
+    });
+    const Education_error = document.getElementById('Education_error')
+    console.log("checkedDetails", checkedDetails);
+    if (checkedDetails == 0) {
+        Education_error.innerHTML = "Education Qualification is required"
+    }else {
+        Education_error.innerHTML = ""
+    }
+
 });
 
-// document.getElementById('submit').onclick = function () {
-//     // Get all checkboxes with the name "option"
-//     const checkboxes = document.querySelectorAll('input[name="option"]');
-    
-//     // Create an array to store the details of checked checkboxes
-//     const checkedDetails = [];
-    
-//     // Loop through each checkbox to check if it's checked
-//     checkboxes.forEach(checkbox => {
-//         if (checkbox.checked) {
-//             // If checked, push its ID and label text to the array
-//             checkedDetails.pop({
-//                 label: checkbox.nextElementSibling.textContent
-//             });
-//         }
-//     });
-    
-//     // Log the details of checked checkboxes
-//     console.log("Checked checkboxes:", checkedDetails.value);
-// };
+
+
 
 
 
